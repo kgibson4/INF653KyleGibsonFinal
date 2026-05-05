585cb0b3ea2cf35c65ec774744222b5490693eb2
Kyle Gibson <mr.gibbi22@yahoo.com>
2026-05-05 09:33:13 -0500

    Connected to /states endpoint and reading state data

--- controllers/statesController.js
+++ controllers/statesController.js
@@ -1,2 +1,26 @@
 const State = require('../models/States');
 const data = require('../data/statesData.json');
+
+const getAllStates = async (req, res) => {
+  try {
+    const statesDB = await State.find();
+
+    const mergedStates = data.map(state => {
+      const match = statesDB.find(
+        db => db.stateCode === state.code
+      );
+
+      return {
+        ...state,
+        funfacts: match ? match.funfacts : []
+      };
+    });
+
+    res.json(mergedStates);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+// Export it correctly
+module.exports = { getAllStates };
