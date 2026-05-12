6fa07af69e45a1638657e72f5904f2b561cd54d0
Kyle Gibson <mr.gibbi22@yahoo.com>
2026-05-11 19:14:52 -0500

    html doc bug fix

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const path = require('path');
 require('dotenv').config();
 
 // Middleware
@@ -14,7 +15,7 @@ mongoose.connect(process.env.DATABASE_URI)
 
 // Simple test route
 app.get('/', (req, res) => {
-  res.send('<h1>States API</h1>');
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.all('*path', (req, res) => {
