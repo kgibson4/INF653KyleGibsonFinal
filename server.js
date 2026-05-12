b6b54a118788eeb059cf85d8671b8082ab65de70
Kyle Gibson <mr.gibbi22@yahoo.com>
2026-05-11 20:03:32 -0500

    par-CORS!

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const mongoose = require('mongoose');
 const path = require('path');
 require('dotenv').config();
 
 // Middleware
+app.use(cors());
 app.use(express.json());
 app.use('/states', require('./routes/states'));
 
